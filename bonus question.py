e7dc62132ab8330fa45b1b4719a91cade67fb393
sreyaghosh092 <46235829+sreyaghosh092@users.noreply.github.com>
2018-12-29 13:23:12 +0530

    bonus question2 by Sreya Ghosh

---
+++ bonus question.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a=int(input("Enter a number: "))
+b=0
+i=0
+while(a!=b):
+    import random
+    b=random.randint(100000,999999)
+    i+=1
+print("No of attempts made to match ur number: ",i)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
